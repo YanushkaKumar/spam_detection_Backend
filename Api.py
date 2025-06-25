c5526da18b9fba9ca28038e3f37682af340528ac
YanushkaKumar <141440648+YanushkaKumar@users.noreply.github.com>
2025-06-25 16:12:37 +0530

    Initial commit: Multilingual Spam Detection API

--- Api.py
+++ Api.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import joblib
 import langid
 import numpy as np
@@ -30,6 +31,7 @@ except:
         NLTK_AVAILABLE = False
 
 app = Flask(__name__)
+CORS(app) 
 
 class ImprovedSpamAPI:
     def __init__(self):
